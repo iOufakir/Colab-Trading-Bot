4b6cc173fec66afb2bcdc2ff05af0e1831db10f3
Ilyas Oufakir <ouf.ilyas@gmail.com>
2024-04-05 17:17:46 -0400

    Send emails using sendgrid

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template_string
 import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
 from nbconvert import HTMLExporter
-import requests
+import requests, subprocess
 import os, sys, logging, schedule, time
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -14,7 +18,7 @@ def home():
 
 
 def schedule_ai_bot():
-    schedule.every(10).minutes.do(run_colab)
+    schedule.every(1).hours.do(run_colab)
     while True:
         schedule.run_pending()
         time.sleep(1)
@@ -29,6 +33,7 @@ def run_colab():
     # Execute the downloaded notebook
     result = execute_notebook(outputFile)
 
+    send_email(result, "Colab Result", "dev@il-yo.com", "ouf.ilyas@gmail.com")
     return render_template_string(result)
 
 
@@ -43,32 +48,41 @@ def download_file(url, output):
     except Exception as e:
         logger.error("Error downloading file", exc_info=True)
 
-
 def execute_notebook(notebook_path):
-    # Load the notebook
-    with open(notebook_path, "r", encoding="utf-8") as f:
-        nb = nbformat.read(f, as_version=4)
-
-    # Get the kernel name from notebook metadata
-    kernel_name = nb.metadata.kernelspec.name
-    logger.info(kernel_name)
-    # Create an ExecutePreprocessor
-    ep = ExecutePreprocessor(timeout=None, kernel_name=kernel_name, allow_errors=True)
-
-    executed_nb = ""
     try:
-        logger.info("Executing notebook...")
-        executed_nb, _ = ep.preprocess(nb, {"metadata": {"path": "./data"}})
+        # Run the Jupyter command to execute the notebook
+        subprocess.run(['jupyter', 'nbconvert', '--to', 'notebook', '--execute', notebook_path])
+
+        # Read the executed notebook
+        with open(notebook_path, "r", encoding="utf-8") as f:
+            executed_nb = f.read()
+
         logger.info("Notebook executed successfully")
+
+        # Convert executed notebook to HTML for display
+        html_exporter = HTMLExporter()
+        html_body, _ = html_exporter.from_notebook_node(executed_nb)
+        return html_body
     except Exception as e:
         execution_result = f"Error executing notebook: {str(e)}"
         logger.info(execution_result)
         return execution_result
 
-    # Convert executed notebook to HTML for display
-    html_exporter = HTMLExporter()
-    html_body, _ = html_exporter.from_notebook_node(executed_nb)
-    return html_body
+
+def send_email(body, subject, sender_email, recipient_email):
+    message = Mail(
+        from_email=sender_email,
+        to_emails=recipient_email,
+        subject=subject,
+        html_content=body)
+
+    try:
+        sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
+        result = sg.send(message)
+        logger.info("Email sent successfully : %s", result.status_code)
+    except Exception as e:
+        logger.error("Error sending email:", e)
+
 
 def _init_logger():
     logger = logging.getLogger('app')
@@ -82,8 +96,10 @@ def _init_logger():
 
 logger = _init_logger()  # Initialize logger
 
+
 if __name__ == "__main__":
+    send_email("Hello", "Colab Result", "dev@il-yo.com", "contact@il-yo.com")
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(port=port)
     schedule_ai_bot()
 
