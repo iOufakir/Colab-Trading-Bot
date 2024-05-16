eb69e6693c9cc9e5e34a177331fbe1cf794014fa
Ilyas Oufakir <ouf.ilyas@gmail.com>
2024-05-15 22:18:10 -0400

    Add gemini API for trading

--- api/app.py
+++ api/app.py
@@ -8,6 +8,12 @@ from threading import Thread
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from dotenv import load_dotenv
+import datetime
+import pathlib
+import textwrap
+import google.generativeai as genai
+from IPython.display import display
+from IPython.display import Markdown
 
 load_dotenv()
 
@@ -15,28 +21,52 @@ app = Flask(__name__)
 
 colabUrl = "https://drive.google.com/uc?id=1wUm_EV7nivXq7JbN7RUeG2E6w9ismXxN"
 outputFile = "./data/smartBot.ipynb"
-    
+
+
+gemini_prompt = """
+Context: As a financial data model, is it a good idea to buy NVDA stock based on the current date/time? Do quick market research, check Yahoo data, analytics and any provided data and give me a quick response.
+
+Details: Reply with one word: positive, neutral or negative. (positive to buy, and negative to sell)
+
+Prompt: Based on the current date/time '%s', should I buy a $NVDA stock right now?
+"""
+
+def get_current_time():
+    current_datetime = datetime.datetime.now()
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d %I:%M %p")
+    return formatted_datetime
+
 @app.route("/")
 def home():
     return "Home Page Route"
 
+@app.route("/api/run-gemini")
+def run_gemini():
+    genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(gemini_prompt)
+    print(response.prompt_feedback)
+    return response.text
 
-@app.route("/api/run-colab", methods=['HEAD'])
+
+@app.route("/api/run-colab", methods=["HEAD"])
 def run_colab():
     # Download file from gdrive
     download_file()
-    
+
     # Background task
     background_task = Thread(target=execute_background_task)
     background_task.start()
 
     return "Colab notebook has been executed!", 202
 
+
 def execute_background_task():
     # Execute the colab notebook
     result = execute_notebook()
     # Send email with the result
     send_email(result, "Colab Result", "dev@il-yo.com", "contact@il-yo.com")
+
 
 def download_file():
     try:
@@ -49,6 +79,7 @@ def download_file():
     except Exception as e:
         logger.error("Error downloading file", exc_info=True)
 
+
 def execute_notebook():
     # Load the notebook
     with open(outputFile, "r", encoding="utf-8") as f:
@@ -57,7 +88,9 @@ def execute_notebook():
     kernel_name = nb.metadata.kernelspec.name
     logger.info(f"kernel: {kernel_name}")
     # Create an ExecutePreprocessor
-    ep = ExecutePreprocessor(timeout=None, kernel_name=kernel_name, store_widget_state=False)
+    ep = ExecutePreprocessor(
+        timeout=None, kernel_name=kernel_name, store_widget_state=False
+    )
 
     executed_nb = ""
     try:
@@ -68,32 +101,33 @@ def execute_notebook():
         execution_result = f"Error executing notebook: {str(e)}"
         logger.info(execution_result)
         return execution_result
-    
+
     # Convert executed notebook to HTML for display
     html_exporter = HTMLExporter()
     html_body, _ = html_exporter.from_notebook_node(executed_nb)
     return html_body
+
 
 def send_email(body, subject, sender_email, recipient_email):
     message = Mail(
         from_email=sender_email,
         to_emails=recipient_email,
         subject=subject,
-        html_content=body)
+        html_content=body,
+    )
 
     try:
-        sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
+        sg = SendGridAPIClient(os.getenv("SENDGRID_API_KEY"))
         result = sg.send(message)
         logger.info("Email sent successfully : %s", result.status_code)
     except Exception as e:
         logger.error("Error sending email:", e)
 
-
 def _init_logger():
-    logger = logging.getLogger('app')
+    logger = logging.getLogger("app")
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter('%(levelname)s - %(message)s')
+    formatter = logging.Formatter("%(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     return logger
@@ -105,4 +139,3 @@ logger = _init_logger()  # Initialize logger
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
